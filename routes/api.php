a28d3d39e94e31643e86d4c0772d6191e9dfd3ae
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-05-27 22:07:52 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 //Task Api
+// Route::group(['middleware' => 'cors'], function () {
+//     Route::Resource('tasks', TaskController::class);
+// });
 
-Route::Resource('tasks', TaskController::class);
 
-
-
-
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'delete']);
